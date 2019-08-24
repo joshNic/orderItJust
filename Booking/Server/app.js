0254426f52105a0a2d2f00562f8e11b14ee74112
Joshua Mugisha <joshuamugisha@MacBook-Pro-90.local>
2019-08-24 13:11:14 +0300

    add user model

--- Booking/Server/app.js
+++ Booking/Server/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+const Event = require('./models/event');
 
 const app = express();
-const events = [];
 
 app.use(bodyParser.json());
 
@@ -43,22 +45,55 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return events;
+        return Event.find()
+          .then(events => {
+            console.log(events);
+            return events.map(event => {
+              return {
+                ...event._doc,
+                _id: event.id
+              };
+            });
+          })
+          .catch(err => {
+            throw err;
+          });
       },
       createEvent: args => {
-        const event = {
-          _id: Math.random().toString(),
+        const event = new Event({
           title: args.eventInput.title,
           description: args.eventInput.description,
           price: +args.eventInput.price,
-          date: args.eventInput.date
-        };
-        events.push(event);
-        return event;
+          date: new Date(args.eventInput.date)
+        });
+        return event
+          .save()
+          .then(result => {
+            console.log(result);
+            return {
+              ...result._doc,
+              _id: result._doc._id.toString()
+            };
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
       }
     },
     graphiql: true
   })
 );
 
-app.listen(3000);
+const httpUrl = `mongodb+srv://${process.env.MONGO_ATLAS_USER}:${
+  process.env.MONGO_ATLAS_PW
+}@cluster0-gmoxm.mongodb.net/bookings`;
+
+mongoose
+  .connect(httpUrl, { useCreateIndex: true, useNewUrlParser: true })
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log('error:', err);
+  });
