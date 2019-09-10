aca338f4de4af49c9c07592fc15925ff023df000
Joshua Mugisha <joshuamugisha@MacBook-Pro-90.local>
2019-09-10 23:28:28 +0300

    add and setup front-end with react

---
+++ Booking/front-end/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import './App.css';
+import Auth from './pages/Auth';
+import Events from './pages/Events';
+import Bookings from './pages/Bookings';
+import MainNavigation from './components/navigation/MainNavigation';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <React.Fragment>
+        <MainNavigation />
+        <main className='main-content'>
+          <Switch>
+            <Redirect from='/' to='auth' exact />
+            <Route path='/auth' component={Auth} />
+            <Route path='/events' component={Events} />
+            <Route path='/bookings' component={Bookings} />
+          </Switch>
+        </main>
+      </React.Fragment>
+    </BrowserRouter>
+  );
+}
+
+export default App;
